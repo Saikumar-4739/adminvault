72f9dbecff5ba8d225011bcdc3467f6c8cf59ff3
saikumar-5450 <ummidisettisai01@gmail.com>
2025-12-16 12:45:23 +0530

    #deploy issues fixed

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from '../database/database.module';
 import { CompanyInfoModule } from './features/company-info/company-info.module';
 import { AuthUsersModule } from './features/auth-users/auth-users.module';
 import { EmployeesModule } from './features/employees/employees.module';
@@ -18,19 +14,13 @@ import { AssetAssignModule } from './features/asset-assign/asset-assign.module';
 import { TicketCommentsModule } from './features/ticket-comments/ticket-comments.module';
 import { TicketStatusLogsModule } from './features/ticket-status-logs/ticket-status-logs.module';
 import { EmailAccountsModule } from './features/email-accounts/email-accounts.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'dist/backend/schema.gql'),
-      sortSchema: true,
-      playground: true,
-      introspection: true,
     }),
     DatabaseModule,
     // CompanyInfoModule,
--- packages/backend/src/app/features/auth-users/auth-users.module.ts
+++ packages/backend/src/app/features/auth-users/auth-users.module.ts
@@ -4,12 +4,11 @@ import { AuthUsersEntity } from '../../entities/auth-users.entity';
 import { AuthUsersService } from './auth-users.service';
 import { AuthUsersController } from './auth-users.controller';
 import { AuthUsersRepository } from '../../repository/auth-users.repository';
-import { AuthUsersResolver } from './auth-users.resolver';
 
 @Module({
     imports: [TypeOrmModule.forFeature([AuthUsersEntity])],
     controllers: [AuthUsersController],
-    providers: [AuthUsersService, AuthUsersRepository, AuthUsersResolver],
+    providers: [AuthUsersService, AuthUsersRepository],
     exports: [AuthUsersService],
 })
 export class AuthUsersModule { }
