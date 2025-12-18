7ad714bfff0222b3d707c2bbebdead103f1cc9c7
saikumar-5450 <ummidisettisai01@gmail.com>
2025-12-18 20:58:15 +0530

    #

---
+++ packages/libs/shared-services/src/common-axios-service.ts
@@ -0,0 +1,50 @@
+import { AxiosRequestConfig } from 'axios';
+import { AxiosInstance } from './axios-instance';
+import { configVariables } from './config';
+
+export class CommonAxiosService {
+    URL = configVariables.APP_AVS_SERVICE_URL;
+
+    private handleResponse = (response: any) => {
+        if (response && (response.status >= 200 && response.status < 300)) {
+            return response.data;
+        } else {
+            throw response;
+        }
+    };
+
+    private handleError = (err: any) => {
+        const errorMessage = err?.response?.data?.message || err?.message || 'An error occurred';
+        throw new Error(errorMessage);
+    };
+
+    axiosPostCall = async (urlEndPoint: string, data?: any, config?: AxiosRequestConfig) => {
+        return await AxiosInstance.post(this.URL + urlEndPoint, data, config)
+            .then(this.handleResponse)
+            .catch(this.handleError);
+    };
+
+    axiosGetCall = async (urlEndPoint: string, config?: AxiosRequestConfig) => {
+        return await AxiosInstance.get(this.URL + urlEndPoint, config)
+            .then(this.handleResponse)
+            .catch(this.handleError);
+    };
+
+    axiosPutCall = async (urlEndPoint: string, data?: any, config?: AxiosRequestConfig) => {
+        return await AxiosInstance.put(this.URL + urlEndPoint, data, config)
+            .then(this.handleResponse)
+            .catch(this.handleError);
+    };
+
+    axiosPatchCall = async (urlEndPoint: string, data?: any, config?: AxiosRequestConfig) => {
+        return await AxiosInstance.patch(this.URL + urlEndPoint, data, config)
+            .then(this.handleResponse)
+            .catch(this.handleError);
+    };
+
+    axiosDeleteCall = async (urlEndPoint: string, config?: AxiosRequestConfig) => {
+        return await AxiosInstance.delete(this.URL + urlEndPoint, config)
+            .then(this.handleResponse)
+            .catch(this.handleError);
+    };
+}
