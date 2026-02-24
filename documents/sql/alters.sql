5ab978bd2e7f0b2e19c2c276048a9353497cdd5c
saikumar5450 <ummidisettisai01@gmail.com>
2026-02-24 12:49:16 +0530

    chore: Remove UI deployment script and sample data, and clean up SQL alter script.

--- documents/sql/alters.sql
+++ documents/sql/alters.sql
@@ -1,6 +1,2 @@
 -- SQL Scripts for Table Alterations
--- Store your ALTER TABLE, CREATE TABLE, and other schema modification scripts here.
 
--- Example:
--- ALTER TABLE auth_users ADD COLUMN google_id VARCHAR(255) NULL UNIQUE;
--- ALTER TABLE auth_users ADD COLUMN microsoft_id VARCHAR(255) NULL UNIQUE;
